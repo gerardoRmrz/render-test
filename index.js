13596dcf32d79f96914c3cbc6bc4c86c36923c28
gerardoRmrz <grmzrso@outlook.com>
2025-12-01 09:30:39 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let agenda = [
+  {
+    "id": 1,
+    "name": "Aato Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-532523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423422"
+  }
+]
+
+app.get('/info', (request, response) => {
+  const length = agenda.length;
+  const date = new Date();
+  const dateStr = `<br><div>${date.toString()}</div>`
+  response.send(`<div>Phonebook has info for ${length} people</div> ${dateStr}`);
+
+})
+
+
+
+const PORT = 3001;
+app.listen(PORT, ()=>{
+  console.log(`Server running on port ${PORT}`);
+});
