f9518a65bf35545aefbd187cd378fbe977c9f43b
gerardoRmrz <grmzrso@outlook.com>
2025-12-01 14:21:16 -0500

    feat: add morgan log for POST requests

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-
-app.use(morgan('tiny'));
 app.use(express.json());
+
+
+app.use(morgan((tokens, req, res)=>{
+  const newPerson = tokens
+      .method(req,res)==='POST' 
+      ? `{"name":"${req.body.name}", "number":"${req.body.number}"}`
+      : null ;  
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms', newPerson
+  ].join(' ')
+}));
+
+
 
 let agenda = [
   {
